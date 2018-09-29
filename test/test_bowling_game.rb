6f5586be88b46aa5bd405844ea86492d72db8cdc
myoan <motoki.yoan@gmail.com>
2018-09-29 14:40:58 +0900

    save score

--- test/test_bowling_game.rb
+++ test/test_bowling_game.rb
@@ -4,7 +4,13 @@ require "./bowling_game"
 class TestBowlingGame < Minitest::Test
   def test_all_roll_are_gutter
     game = BowlingGame.new
-    game.roll(0)
+    20.times { game.roll(0) }
     assert_equal game.score, 0
+  end
+
+  def test_all_roll_are_1
+    game = BowlingGame.new
+    20.times { game.roll(1) }
+    assert_equal game.score, 20
   end
 end
